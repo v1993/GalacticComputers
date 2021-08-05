2e636e7745cb6cef186c0b639c85c24619cb2bdf
v1993 <v19930312@gmail.com>
2021-08-05 16:50:26 +0300

    Prepare for 1.0.0 release
    Dedicated server turned out to work without further tweaking!

--- src/main/java/com/v1993/galacticcomputers/GalacticComputers.java
+++ src/main/java/com/v1993/galacticcomputers/GalacticComputers.java
@@ -27,7 +27,7 @@ import com.v1993.galacticcomputers.gccore.GCCoreSubmod;
 public class GalacticComputers {
 	public static final String MODID = "galacticcomputers";
 	public static final String NAME = "Galactic Computers";
-	public static final String VERSION = "0.1";
+	public static final String VERSION = "1.0.0";
 
 	private static Logger logger;
 
